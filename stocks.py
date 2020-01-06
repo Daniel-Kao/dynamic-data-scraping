b2dc44b145399650678ed90604e619b049ba4987
Daniel-Kao <396007476@qq.com>
2020-01-06 21:02:37 +0800

    init

---
+++ stocks.py
@@ -0,0 +1,41 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+import re
+import sys
+from PyQt5.QtWebEngineWidgets import QWebEnginePage
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QUrl
+
+class Page(QWebEnginePage):
+    def __init__(self, url):
+        self.app = QApplication(sys.argv)
+        QWebEnginePage.__init__(self)
+        self.html = ''
+        self.loadFinished.connect(self._on_load_finished)
+        self.load(QUrl(url))
+        self.app.exec_()
+
+    def _on_load_finished(self):
+        self.html = self.toHtml(self.Callable)
+        print('Load finished')
+
+    def Callable(self, html_str):
+        self.html = html_str
+        self.app.quit()
+
+def main():
+    page = Page('http://quote.eastmoney.com/center/gridlist.html#hs_a_board')
+    soup = BeautifulSoup(page.html, 'lxml')
+    pattern = r'http://quote\.eastmoney.com/kcb/688081.html'
+    codeList = soup.findAll('a', href=pattern)
+    print(codeList)
+
+
+if __name__ == '__main__': main()
+
+
+
+
+
+# getStockCodes(url)
